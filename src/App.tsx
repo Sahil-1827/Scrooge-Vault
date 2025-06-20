2ac9566ba96f77fbf6e80985b37cde2d63a52f08
Sahil-1827 <sga.opash@gmail.com>
2025-06-20 13:12:59 +0530

    remove extra px py

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import VaultClaimModule from "./components/organisms/VaultClaimModule";
 
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center py-4 px-2 sm:px-4 bg-[#211A2C]">
+    <div className="min-h-screen flex items-center justify-center bg-[#211A2C]">
       <VaultClaimModule />
     </div>
   );
